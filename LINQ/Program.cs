d4d81ba476171ed45460066b2cd59f139677927e
Maxim Troshkov <maxitm_troshkov@mail.ru>
2018-11-02 14:32:59 +0300

    LINQ requests

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -8,8 +8,41 @@ namespace LINQ
 {
     class Program
     {
+        class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Sex { get; set; }
+            public int Balance { get; set; }
+        }
+
+        static List<Person> people = new List<Person>
+        {
+            new Person { Name="Иван", Age=31, Sex = "Male", Balance = 400 },
+            new Person { Name="Женя", Age=24, Sex = "Male", Balance = 21000 },
+            new Person { Name="Даша", Age=22, Sex = "Female", Balance = 570 },
+            new Person { Name="Леша", Age=25, Sex = "Male", Balance = 14758 },
+            new Person { Name="Соня", Age=27, Sex = "Female", Balance = 4792 }
+        };
+
         static void Main(string[] args)
         {
+
+            //Задание2
+
+            var quantity = people.Where(x => x.Balance > 4000).Count();
+            Console.WriteLine("{0} {1}", "Ко-во людей с балансом выше 4000:", quantity);
+            int.TryParse(Console.ReadLine(), out var nextTask);
+
+            //Задание3
+
+            var result = people
+                .OrderBy(x => x.Age)
+                .ThenBy(x => x.Sex)
+                .ThenBy(x => x.Balance);
+            foreach (Person person in result)
+                Console.WriteLine("{0} - {1}", person.Name, person.Age);
+            int.TryParse(Console.ReadLine(), out var exit);
         }
     }
 }
